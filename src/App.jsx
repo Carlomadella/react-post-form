4d097a0c7fa51bf5203a66d7dd2a2234ebd265e6
Carlomadella <madella871@gmail.com>
2025-05-19 00:08:46 +0200

    [UPD] creata funzione per gestire l'eveneto onChange

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,15 @@ function App() {
     published: false
   })
 
+  const handleChange = (e) => {
+    const {name, value, type, checked} = e.target
+    
+    const newData = { ...formData };
+
+    newData[name] = type === 'checkbox' ? checked : value;
+    setFormData(newData)
+  };
+
   return (
     <>
       <div className="container">
@@ -30,26 +39,27 @@ function App() {
                     className="form-control"
                     placeholder="Autore"
                     value={formData.author}
+                    onChange={handleChange}
                   />
                 </div>
                 <div className="col-12">
                   <label htmlFor="" className="control-label">
                     Titolo
                   </label>
-                  <input type="text" name="title" id="title" className="form-control" placeholder="Titolo" value={formData.title}/>
+                  <input type="text" name="title" id="title" className="form-control" placeholder="Titolo" value={formData.title} onChange={handleChange}/>
                 </div>
                 <div className="col-12">
                   <label htmlFor="" className="control-label">
                     Corpo dell'articolo
                   </label>
-                  <textarea name="body" id="body" className="form-control" placeholder="Corpo dell'articolo" value={formData.body}></textarea>
+                  <textarea name="body" id="body" className="form-control" placeholder="Corpo dell'articolo" value={formData.body} onChange={handleChange}></textarea>
                 </div>
                 <div className="col-12">
                   <div className="form-check">
                     <label htmlFor="" className="form-check-label">
                       Pubblicato
                     </label>
-                    <input type="checkbox" name="published" id="published" className="form-check-input" />
+                    <input type="checkbox" name="published" id="published" className="form-check-input" checked={formData.published} onChange={handleChange}/>
                   </div>
                 </div>
                 <div className="col-12">
